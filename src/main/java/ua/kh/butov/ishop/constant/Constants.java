91bdbc7cd9a17dece49f101ec299c5ec2246b1d3
Viacheslav Butov <vbutov91@gmail.com>
2017-03-15 20:39:24 +0200

    Application constant added

--- src/main/java/ua/kh/butov/ishop/constant/Constants.java
+++ src/main/java/ua/kh/butov/ishop/constant/Constants.java
@@ -12,6 +12,8 @@ public final class Constants {
 	public static final int MAX_PRODUCTS_PER_SHOPPING_CART = 20;
 
 	public static final String ACCOUNT_ACTIONS_HISTORY = "ACCOUNT_ACTIONS_HISTORY";
+	
+	public static final int MAX_PRODUCTS_PER_HTML_PAGE = 12;
 
 	public enum Cookie {
 		// 1 year ttl
